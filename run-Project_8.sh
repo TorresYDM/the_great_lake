dd91f17e31f0a322bd609e1de3b9701ed3d6b56d
TorresYDM <90394805+TorresYDM@users.noreply.github.com>
2021-12-10 13:52:05 -0500

    Update run-Project_8.sh

--- run-Project_8.sh
+++ run-Project_8.sh
@@ -5,14 +5,14 @@
 # 79: -------------------------------------------------------------------------
 
 # slurm options: --------------------------------------------------------------
-#SBATCH --job-name=Project8.py
+#SBATCH --job-name=Project_8.py
 #SBATCH --mail-user=dongming@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --cpus-per-task=5
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=5GB 
-#SBATCH --time=120:00
+#SBATCH --time=30:00
 #SBATCH --account=cscar
 #SBATCH --partition=standard
 #SBATCH --output=/home/%u/logs/%x-%j-4.log
