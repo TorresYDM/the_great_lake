0ea6a12b40c4a4af11eadac0da3800b7dcb80f3f
TorresYDM <90394805+TorresYDM@users.noreply.github.com>
2021-12-12 21:16:39 -0500

    Update run-Project_8.sh

--- run-Project_8.sh
+++ run-Project_8.sh
@@ -13,7 +13,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=5GB 
 #SBATCH --time=30:00
-#SBATCH --account=cscar
+#SBATCH --account=stats507f21_class
 #SBATCH --partition=standard
 #SBATCH --output=/home/%u/logs/%x-%j-4.log
 
